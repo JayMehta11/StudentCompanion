ba7ac849f2995e7420979dd6cb27338866c200b6
JayMehta11 <jay.m@ahduni.edu.in>
2021-07-25 22:33:50 +0530

    final commit

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express.Router();
-
-app.use('/auth',require('./auth.api'));
-app.use('/todo',require('./todo.api'));
-app.use('/course',require('./course.api'));
-
-module.exports = app
